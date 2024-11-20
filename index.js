df723b13231983ba681965dcf09fe7aeef0dbd8d
Priyanshu Rajak <158009564+Priyanshuu7@users.noreply.github.com>
2024-11-20 22:51:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+
+// server code //
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
